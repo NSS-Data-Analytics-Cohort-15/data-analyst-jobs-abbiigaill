487dd64697f20aa1c15901f264d63675f8b8f8fa
Abbiigaill <abigail.spence@myyahoo.com>
2025-06-07 10:33:59 -0500

    completed all questions

--- scripts/data_analyst_job_project.sql
+++ scripts/data_analyst_job_project.sql
@@ -44,8 +44,9 @@ WHERE review_count BETWEEN 500 AND 1000
 --Question6
 SELECT AVG (star_rating),location
 FROM data_analyst_jobs2
-GROUP BY location
---KS 
+GROUP BY location 
+ORDER BY avg DESC;
+--NE
 
 --Question7
 SELECT COUNT (DISTINCT title)
@@ -61,11 +62,51 @@ WHERE LOCATION='CA'
 --Question9
 SELECT company,AVG(star_rating) AS average_star_rating, MAX(review_count) AS total_review_count
 FROM data_analyst_jobs2
-WHERE review_count > 5000
+WHERE review_count > 5000 AND company IS NOT NULL
 GROUP BY company
 ORDER BY total_review_count DESC;
---41
+--40
+
+
 --Question10
+SELECT company,AVG(star_rating) AS average_star_rating, MAX(review_count) AS total_review_count
+FROM data_analyst_jobs2
+WHERE review_count > 5000
+GROUP BY company
+ORDER BY average_star_rating DESC;
+--General Motors, Unilever,microsoft,american express,kaiser permanente with an average of 4.19
+
+--Question11
+SELECT COUNT(title),title
+FROM data_analyst_jobs2
+Where title ILIKE '%Analyst%' 
+GROUP BY title
+--774
+
+--QUESTION12
+SELECT title
+FROM data_analyst_jobs2
+WHERE title NOT ILIKE '%Analyst%' AND title NOT ILIKE '%Analytics%'
+--4, Tableau
+
+--BONUS QUESTIONS
+--1. 
+SELECT DOMAIN, COUNT(*) AS domain_jobs
+FROM data_analyst_jobs2
+WHERE skill LIKE '%SQL%'
+AND days_since_posting>21
+AND DOMAIN IS NOT null
+GROUP BY domain
+ORDER BY domain_jobs DESC
+LIMIT 3;
+--Internet and Software(62), Banks and Finacial Services(61), Consulting and Busniess Services(57) 
+
+
+
+
+
+
+
 
 
 
